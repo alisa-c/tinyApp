79ac91e99e9282d2ca1a619abd4463c84fcf6ebe
alisa-c <alisachuvilskaya@gmail.com>
2018-10-31 16:01:33 +0000

    nodemon added
    comments updated

--- express_server.js
+++ express_server.js
@@ -1,17 +1,19 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.set("view engine", "ejs");
 
+//creates random 6 character for the shorturl
 function generateRandomString(url) {
   let random = "";
   let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   character.split('');
-  for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     random += character.charAt(Math.floor(Math.random() * character.length));
   }
   return random;
@@ -22,34 +24,29 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//shows the long urls in the database
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//adds a longURL with its created shortURL into the database
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString(req.body.longURL);
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`);
+});
+
+//page where you add url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString(req.body.longURL);
-  urlDatabase[shortURL] = req.body.longURL
-  console.log(urlDatabase);// debug statement to see POST parameters
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
-});
-
+//shows the corresponding longURL to the shortURL (id)
 app.get("/urls/:id", (req, res) => {
   let longURL = urlDatabase[req.params.id]
   let templateVars = { shortURL: req.params.id,
@@ -58,11 +55,9 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-
+//finds given shortURL if in database redircts to corresponding longURL
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]
-  console.log(longURL);
   res.redirect(longURL);
 });
 
